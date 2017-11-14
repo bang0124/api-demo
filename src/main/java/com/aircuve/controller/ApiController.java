68ff47c14be405abc72695d3677804b334d2fc42
bangseungho <bangseungho@10.10.60.102>
2017-11-14 10:20:01 +0900

    edit source

--- src/main/java/com/aircuve/controller/ApiController.java
+++ src/main/java/com/aircuve/controller/ApiController.java
@@ -1,21 +1,34 @@
 package com.aircuve.controller;
 
 import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value="/api") 
+@RestController
 public class ApiController {
 	
+	private static String userId = "aircuvetest";
+	private static String userPwd = "password";
 	
-//	@RequestMapping(value="/home", method=RequestMethod.GET) 
-//	public String home() {
-//		
-//		JSONObject json = new JSONObject();
-//		
-//		
-//		return json.toString();
-//	}
+	
+	@RequestMapping(value="/login/request", method=RequestMethod.GET) 
+	public String login(@RequestBody String body) {
+		
+		JSONObject bodyJson = new JSONObject(body);
+		JSONObject resultJson = new JSONObject();
+		
+		
+		if(userId.equals(bodyJson.get("userId")) && userPwd.equals(bodyJson.get("userPwd"))){
+			resultJson.put("success", "true");
+		}else{
+			resultJson.put("success", "false");
+		}
+		
+		
+		
+		return resultJson.toString();
+	}
 	
 }
